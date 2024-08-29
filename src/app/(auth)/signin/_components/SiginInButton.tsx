54c09cc70c535271c253af1674a49e24089f948c
theSovereignProject <127284370+Nikhar-savaliya@users.noreply.github.com>
2024-08-29 11:29:32 +0530

    add sigin route

---
+++ src/app/(auth)/signin/_components/SiginInButton.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import Image from "next/image";
+import React from "react";
+import googleLogo from "@/assets/google-icon.svg";
+
+const SiginInButton = () => {
+  const handleSignIn = async () => {
+    const user = await signIn("google");
+  };
+  return (
+    <Button
+      type="submit"
+      onClick={handleSignIn}
+      className="w-full flex gap-4 items-center"
+    >
+      <Image src={googleLogo} alt={"Google logo"} width={20} height={20} />
+      Register with Google
+    </Button>
+  );
+};
+
+export default SiginInButton;
---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+import SiginInButton from "./_components/SiginInButton";
+
+function Signin() {
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px] pt-28 md:pt-0">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Sign In</h1>
+            <p className="text-balance text-muted-foreground">
+              Sign in to participate in Updates 2k24.
+            </p>
+          </div>
+          {/* <div className="grid gap-4"></div> */}
+          <SiginInButton />
+          <div className="mt-4 text-center text-sm">
+            Navigate back to{" "}
+            <Link href="/" className="underline">
+              Home
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:block">
+        <Image
+          src="/placeholder.svg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Signin;
