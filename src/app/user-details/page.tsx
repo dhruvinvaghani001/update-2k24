ae8bcdf139bafada94d1ac41b760aa2cb6277963
abhiradadiya07 <113245680+abhiradadiya07@users.noreply.github.com>
2024-08-30 10:37:29 +0530

    event page

--- src/app/user-details/page.tsx
+++ src/app/user-details/page.tsx
@@ -15,6 +15,8 @@ import {
 import axios from "axios";
 import { Combobox } from "@/components/ui/combobox";
 import { Input } from "@/components/ui/input";
+import Title from "@/components/Title";
+import { BorderBeam } from "@/components/magicui/border-beam";
 
 const formSchema = z.object({
   department: z.string(),
@@ -43,100 +45,104 @@ const Page = (props: Props) => {
 
   return (
     <div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="year"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>year</FormLabel>
-                <FormControl>
-                  <Combobox
-                    options={["1", "2", "3", "4"]}
-                    {...field}
-                    label="Select Year"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="department"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>department</FormLabel>
-                <FormControl>
-                  <Combobox
-                    options={["CO", "IT", "AIDS", "EL", "MH", "CL", "CH"]}
-                    {...field}
-                    label="Select Department"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="semester"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>semester</FormLabel>
-                <FormControl>
-                  <Combobox
-                    options={["1", "2", "3", "4", "5", "6", "7", "8"]}
-                    {...field}
-                    label="Select Semester"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+      <Title title="User Details"></Title>
+      <div className="max-w-3xl mx-auto ">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="year"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Year : </FormLabel>
+                  <FormControl>
+                    <Combobox
+                      options={["1", "2", "3", "4"]}
+                      {...field}
+                      label="Select Year"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="department"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Department : </FormLabel>
+                  <FormControl>
+                    <Combobox
+                      options={["CO", "IT", "AIDS", "EL", "MH", "CL", "CH"]}
+                      {...field}
+                      label="Select Department"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="semester"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Semester : </FormLabel>
+                  <FormControl>
+                    <Combobox
+                      options={["1", "2", "3", "4", "5", "6", "7", "8"]}
+                      {...field}
+                      label="Select Semester"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <FormField
-            control={form.control}
-            name="phoneNumber"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Mobile Number</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="enter your phone number."
-                    inputMode="numeric"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+            <FormField
+              control={form.control}
+              name="phoneNumber"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Mobile Number</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="enter your phone number."
+                      inputMode="numeric"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <FormField
-            control={form.control}
-            name="enrollmentNo"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Enrollment No.</FormLabel>
-                <FormControl>
-                  <Input
-                    placeholder="enter your enrollment number."
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+            <FormField
+              control={form.control}
+              name="enrollmentNo"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Enrollment No.</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="enter your enrollment number."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
+            <Button className="mx-auto " type="submit">
+              Submit
+            </Button>
+          </form>0
+        </Form>
+      </div>
     </div>
   );
 };
-
 export default Page;
